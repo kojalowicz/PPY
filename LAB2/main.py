dd7606a67a89c46b1059f1e4947ed57eaada4d31
Daniel Yaroshovets <dan.yaroshovets@gmail.com>
2024-03-15 15:15:30 +0100

    made the 5th task

--- LAB2/main.py
+++ LAB2/main.py
@@ -108,31 +108,40 @@ Extend the previous Python program to demonstrate the manipulation of lists usin
   ```
 """
 
-# Input
+# # Input
 # input_numbers = input("Enter a series of space-separated integers: ")
 
-# Convert Input
+# # Convert Input
+# numbers_list = [int(x) for x in input_numbers.split()]
+# numbers_tuple = tuple(numbers_list)
 
+# # Manipulate List
+# #   Append 10 to the list
+# numbers_list.append(10)
 
-# Manipulate List
-#   Append 10 to the list
-#   Insert 20 at index 2
-#   Remove one elementfrom the list
+# #   Insert 20 at index 2
+# numbers_list.insert(2,20)
 
-# Attempt to Modify Tuple (this will raise an error)
+# #   Remove one elementfrom the list
+# numbers_list.remove(7)
+
+# # Attempt to Modify Tuple (this will raise an error)
 # try:
 #     #   Append 10 to the tuple
+#     numbers_tuple.append(10)
 # except AttributeError:
 #     print("Tuples are immutable and cannot be modified.")
 # try:
 #     #   Insert 20 at index 2
+#     numbers_tuple.insert(2,20)
 # except AttributeError:
 #     pass  # Insert operation will also raise an error
 # try:
 #     #   Remove one element  from the tuple
+#     numbers_tuple.remove(7)
 # except AttributeError:
 #     pass  # Remove operation will also raise an error
-#
+
 # # Print Output
 # print("Modified list:", numbers_list)
 # print("Tuple remains unchanged:", numbers_tuple)
