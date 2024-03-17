15db86e96e10cd8b6477e1edac029d5a406b4c99
Marian Didukh <mardidukh@gmail.com>
2024-03-18 01:34:39 +0200

    task-2

--- LAB3/python_lab3/main.py
+++ LAB3/python_lab3/main.py
@@ -2,3 +2,10 @@
 square_num = [x ** 2 for x in range(1, 11)]
 print(square_num)
 
+# 2 Task
+def e_squares(start, end):
+    return [x ** 2 for x in range(start, end)]
+
+
+print(e_squares(3, 15))
+
