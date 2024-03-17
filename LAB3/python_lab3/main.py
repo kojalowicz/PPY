93291a6d25247bffe72df2c583d2e7e8f0b96f22
Marian Didukh <mardidukh@gmail.com>
2024-03-18 01:32:34 +0200

    task-1

--- LAB3/python_lab3/main.py
+++ LAB3/python_lab3/main.py
@@ -1,16 +1,4 @@
-# This is a sample Python script.
+# 1 task
+square_num = [x ** 2 for x in range(1, 11)]
+print(square_num)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
